fc57a79d6a79e1e191c4b8c3c903b359201781a1
viveloper <ucsik1234@gmail.com>
2025-01-29 09:15:08 +0900

    feat: add add-snapshot component

---
+++ src/app/snapshots/components/add-snapshot/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DatePickerForm } from "@/app/snapshots/components/add-snapshot/date-picker-form";
+
+export function AddSnapshot() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>스냅샷 추가</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>스냅샷 추가</DialogTitle>
+          <DialogDescription>
+            스냅샷을 추가하세요. 기준 날짜를 선택하고 추가를 클릭하세요.
+          </DialogDescription>
+        </DialogHeader>
+        <DatePickerForm />
+      </DialogContent>
+    </Dialog>
+  );
+}
