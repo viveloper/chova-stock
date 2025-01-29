a8523c710b8b10e9fae7948a1cf14cb222fd85c3
viveloper <ucsik1234@gmail.com>
2025-01-29 09:15:47 +0900

    edit: change snapshot page

--- src/app/snapshots/page.tsx
+++ src/app/snapshots/page.tsx
@@ -1,23 +1,25 @@
-import { Button } from "@/components/ui/button";
+import { AddSnapshot } from "@/app/snapshots/components/add-snapshot";
 
 export default function Page() {
   return (
     <div className="p-4">
-      <Button>스냅샷 추가</Button>
-      <ul>
-        <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
-          2025-01-28
-        </li>
-        <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
-          2024-12-31
-        </li>
-        <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
-          2024-11-30
-        </li>
-        <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
-          2024-10-28
-        </li>
-      </ul>
+      <div className="space-y-4">
+        <AddSnapshot />
+        <ul className="text-lg space-y-1">
+          <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
+            2025-01-28
+          </li>
+          <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
+            2024-12-31
+          </li>
+          <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
+            2024-11-30
+          </li>
+          <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
+            2024-10-28
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
