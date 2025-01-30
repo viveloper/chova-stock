0b8f36998d56ab6434f3adb6ffc0b6f193f0ede8
viveloper <ucsik1234@gmail.com>
2025-01-31 00:31:11 +0900

    feat: add snapshot detail page link

--- src/app/snapshots/page.tsx
+++ src/app/snapshots/page.tsx
@@ -1,24 +1,12 @@
 import { AddSnapshot } from "@/app/snapshots/components/add-snapshot";
+import { SnapshotList } from "@/app/snapshots/components/snapshot-list";
 
 export default function Page() {
   return (
     <div className="p-4">
       <div className="space-y-4">
         <AddSnapshot />
-        <ul className="text-lg space-y-1">
-          <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
-            2025-01-28
-          </li>
-          <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
-            2024-12-31
-          </li>
-          <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
-            2024-11-30
-          </li>
-          <li className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer">
-            2024-10-28
-          </li>
-        </ul>
+        <SnapshotList />
       </div>
     </div>
   );
