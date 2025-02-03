4b5425ff508c6ac49de68453858e27f12bc513cd
viveloper <ucsik1234@gmail.com>
2025-02-03 21:50:12 +0900

    refactor: use data props for snapshot-list

--- src/app/snapshots/components/snapshot-list.tsx
+++ src/app/snapshots/components/snapshot-list.tsx
@@ -1,41 +1,20 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { Snapshot } from "@/app/snapshots/page";
 
-const data = [
-  {
-    id: 5,
-    name: "2025-01-28",
-  },
-  {
-    id: 4,
-    name: "2024-12-31",
-  },
-  {
-    id: 3,
-    name: "2024-11-30",
-  },
-  {
-    id: 2,
-    name: "2024-10-28",
-  },
-  {
-    id: 1,
-    name: "2024-09-30",
-  },
-];
-
-export function SnapshotList() {
+export function SnapshotList({ snapshots }: { snapshots: Snapshot[] }) {
   const router = useRouter();
   return (
     <ul className="text-lg space-y-1">
-      {data.map((snapshot) => (
-        <li
-          key={snapshot.id}
-          className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer"
-          onClick={() => router.push("/snapshots/" + snapshot.id)}
-        >
-          {snapshot.name}
+      {snapshots.map((snapshot) => (
+        <li key={snapshot.id}>
+          <span
+            className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer"
+            onClick={() => router.push("/snapshots/" + snapshot.id)}
+          >
+            {snapshot.name}
+          </span>
         </li>
       ))}
     </ul>
--- src/app/snapshots/page.tsx
+++ src/app/snapshots/page.tsx
@@ -1,12 +1,42 @@
 import { AddSnapshot } from "@/app/snapshots/components/add-snapshot";
 import { SnapshotList } from "@/app/snapshots/components/snapshot-list";
 
+export type Snapshot = {
+  id: number;
+  name: string;
+};
+
+const data: { snapshots: Snapshot[] } = {
+  snapshots: [
+    {
+      id: 5,
+      name: "2025-01-28",
+    },
+    {
+      id: 4,
+      name: "2024-12-31",
+    },
+    {
+      id: 3,
+      name: "2024-11-30",
+    },
+    {
+      id: 2,
+      name: "2024-10-28",
+    },
+    {
+      id: 1,
+      name: "2024-09-30",
+    },
+  ],
+};
+
 export default function Page() {
   return (
     <div className="p-4">
       <div className="space-y-4">
         <AddSnapshot />
-        <SnapshotList />
+        <SnapshotList snapshots={data.snapshots} />
       </div>
     </div>
   );
