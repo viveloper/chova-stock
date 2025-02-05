bfd83f9b72830bcc5e188aa88583539338447f3e
viveloper <ucsik1234@gmail.com>
2025-02-05 23:23:27 +0900

    feat: use snapshot detail page params

--- src/app/snapshots/[id]/page.tsx
+++ src/app/snapshots/[id]/page.tsx
@@ -1,20 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { TickerList } from "@/app/snapshots/[id]/components/ticker-list";
-import { Ticker } from "@/app/api/snapshots/[id]/route";
 import { APP_ENV } from "@/env";
+import { SnapshotDetail } from "@/app/api/snapshots/[id]/route";
 
-export default async function Page() {
-  const data = await fetch(`${APP_ENV.API_URL}/api/snapshots/1`);
-  const tickers: Ticker[] = await data.json();
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const snapshotId = (await params).id;
+  console.log("snapshotId", snapshotId);
+  const data = await fetch(`${APP_ENV.API_URL}/snapshots/${snapshotId}`);
+  const snapshotDetail: SnapshotDetail = await data.json();
 
   return (
     <div className="p-4">
       <div className="space-y-6">
         <div className="flex items-center justify-between">
-          <div className="text-xl">2025-01-28</div>
+          <div className="text-xl">{`${snapshotDetail.name}(${snapshotDetail.id})`}</div>
           <Button>종목 추가</Button>
         </div>
-        <TickerList tickers={tickers} />
+        <TickerList tickers={snapshotDetail.tickers} />
       </div>
     </div>
   );
