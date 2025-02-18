3f51ccc5e1d40fed99abde77f5c8580b06e5de82
viveloper <ucsik1234@gmail.com>
2025-02-19 01:18:02 +0900

    refactor: no result message (snapshot-list)

--- src/app/snapshots/components/snapshot-list.tsx
+++ src/app/snapshots/components/snapshot-list.tsx
@@ -7,16 +7,20 @@ export function SnapshotList({ snapshots }: { snapshots: Snapshot[] }) {
   const router = useRouter();
   return (
     <ul className="text-lg space-y-1">
-      {snapshots.map((snapshot) => (
-        <li key={snapshot.id}>
-          <span
-            className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer"
-            onClick={() => router.push("/snapshots/" + snapshot.id)}
-          >
-            {`${snapshot.name} (${snapshot.id})`}
-          </span>
-        </li>
-      ))}
+      {snapshots.length > 0 ? (
+        snapshots.map((snapshot) => (
+          <li key={snapshot.id}>
+            <span
+              className="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:cursor-pointer"
+              onClick={() => router.push("/snapshots/" + snapshot.id)}
+            >
+              {`${snapshot.name} (${snapshot.id})`}
+            </span>
+          </li>
+        ))
+      ) : (
+        <div>검색 결과가 없습니다.</div>
+      )}
     </ul>
   );
 }
